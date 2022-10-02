b250814ef54906e4d6cbca7ebeceba8dad644449
Daniel L. Lau <dllau@uky.edu>
2022-10-01 21:25:58 -0400

    Fixes for everything.  Totally works now.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setOrganizationName(QString("Lau Consulting Inc"));
+    a.setOrganizationDomain(QString("drhalftone.com"));
+    a.setApplicationName(QString("LAUWebPowerSwitch"));
+
     LAURemotePowerDialog w;
     return w.exec();
 }
